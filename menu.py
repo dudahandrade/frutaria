2218ccc82da489325991fd0bf06b985e1e65e1c8
dudahandrade <130777294+dudahandrade@users.noreply.github.com>
2024-11-06 12:04:12 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,29 +1,43 @@
-class Fruta:
-    def __init__(self):
-        self.estoque = {}
+import os
+import json
 
-    def cadastrar_fruta(self):
-        nome_da_fruta = input("Qual o nome da fruta: ")
-        qual_unidade_de_venda = input("Qual unidade de venda (kg, un): ")
-        quantidade_da_fruta = input("Qual a quantidade: ")
-        qual_o_preco = input("Qual o valor de venda: ")
+def LimpaTela():
+    os.system('cls' if os.name=='nt' else'clear')
 
-    def adicionar_fruta(self, nome, unidade, quantidade, preco):
-        if nome in self.estoque:
-            self.estoque[nome]['quantidade'] += quantidade
-        else:
-            self.estoque[nome] = {'nome': nome, 'unidade': unidade, 'quantidade': quantidade, 'preco': preco}
-        print(f"Adicionadas {quantidade} unidades de {nome} ao estoque.")
+from cad_fruta import Fruta 
+frutinhas = Fruta()
 
-    def ver_estoque(self):
-        print("\nEstoque de Frutas:")
-        for fruta, info in self.estoque.items():
-            print(f"{fruta}: {info['quantidade']} unidades a R${info['preco']:.2f} cada")
+acessopemitido = False
+usuario = "adm"
+senha = "adm"
 
-    def vender_fruta(self, nome, quantidade):
-        if nome in self.estoque and self.estoque[nome]['quantidade'] >= quantidade:
-            total = quantidade * self.estoque[nome]['preco']
-            self.estoque[nome]['quantidade'] -= quantidade
-            print(f"Vendeu {quantidade} unidades de {nome}. Total: R${total:.2f}")
-        else:
-            print(f"Não há estoque suficiente de {nome}.")
+qusuario = input("Digite seu usuario: ")
+qsenha = input("Digite sua senha: ") 
+
+if (senha == qsenha and usuario == qusuario):
+    acessopermitido = True
+else: 
+    print("Acesso negado")
+
+while acessopermitido:
+    LimpaTela()
+    print("+----------------------+")
+    print("|      Menu            |")
+    print("|                      |")
+    print("|  1 - Cadastro        |")
+    print("|  2 - Mostrar estoque |")
+    print("| 10 - Sair            |")
+    print("|                      |")
+    print("+----------------------+")
+    menu = input(" qual opção: ")
+    if menu == "1":
+        LimpaTela()
+        frutinhas.cadastrar_fruta()
+    elif menu == "2":
+        LimpaTela()
+        frutinhas.ver_estoque()    
+    elif menu == "10":
+        acessopermitido = False
+    else:
+        print("opção invalida")
+
